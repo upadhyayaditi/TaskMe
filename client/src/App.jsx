c3d16e940b31b54455ba29c2cccfa3d63f8d27d4
upadhyayaditi <au.aditi08@gmail.com>
2024-04-08 23:16:32 +0530

    textbox component is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,58 @@
-import Login  from "./pages/Login"
+import { Navigate, Route, Routes, useLocation, Outlet } from "react-router-dom"
+import {Toaster} from "sonner"
+import Dashbaord from "./pages/Dashboard"
+import Login from "./pages/Login"
+import TaskDetails from "./pages/TaskDetails"
+import Tasks from "./pages/Tasks"
+import Trash from "./pages/Trash"
+import Users from "./pages/Users"
+
+function Layout() {
+  const user = ""
+
+  const location = useLocation()
+
+  return user ? (
+    <div className=" w-full h-screen flex flex-col md:flex-row  ">
+      <div className=" w-1/5 h-screen bg-white sticky top-0 hidden md:block ">
+        {/* <Sidebar /> */}
+      </div>
+      {/* <MobileSidebar /> */}
+
+      <div className=" flex-1 overflow-y-auto  ">
+        {/* <Navbar /> */}
+        <div className=" p-4 2xl:px-10  ">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate  to="/log-in" state={{from:location}} replace />
+  )
+}
 
 function App() {
-  return <Login />
+  return (
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Navigate to="/dashboard"/>} />
+          <Route path="/dashboard >" element={<Dashbaord />} />
+          <Route path="/tasks >" element={<Tasks />} />
+          <Route path="/completed/:status >" element={<Tasks />} />
+          <Route path="/in-progress/:status >" element={<Tasks />} />
+          <Route path="/todo/:status >" element={<Tasks />} />
+          <Route path="/team >" element={<Users />} />
+          <Route path="/trashed >" element={<Trash />} />
+          <Route path="/task/:id >" element={<TaskDetails />} />
+        </Route>
+
+        <Route path="/log-in" element={<Login />} />
+    </Routes>
+
+    <Toaster richColors />
+  </main>
+  )
 }
 
 export default App
