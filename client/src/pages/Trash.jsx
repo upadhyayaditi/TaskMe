0f4d85c8fb8f007b7a08687ae716074fc4dda43e
upadhyayaditi <au.aditi08@gmail.com>
2024-04-10 23:21:05 +0530

    trash page is updated and adduser component created

--- client/src/pages/Trash.jsx
+++ client/src/pages/Trash.jsx
@@ -11,8 +11,8 @@ import { tasks } from "../assets/data";
 import Title from "../components/Title";
 import Button from "../components/Button";
 import { PRIOTITYSTYELS, TASK_TYPE } from "../utils";
-import AddUser from "../components/AddUser";
-import ConfirmatioDialog from "../components/Dialogs";
+// import AddUser from "../components/AddUser";
+// import ConfirmatioDialog from "../components/Dialogs";
 
 const ICONS = {
   high: <MdKeyboardDoubleArrowUp />,
