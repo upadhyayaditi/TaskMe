eb1b85fd38b59fa575318a64ab325f099dbc7290
upadhyayaditi <au.aditi08@gmail.com>
2024-04-11 14:30:52 +0530

    db commnection established

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import morgan from "morgan";
+import dbConnection from "./utils/index.js"
+
+
+const routes = "";
+
+dotenv.config()
+
+dbConnection()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(
+    cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    })
+);
+
+app.use(express.json())
+
+app.use(express.urlencoded())
+
+app.use(cookieParser())
+
+app.use(morgan("dev"))
+
+// app.use("/api", routes);
+
+// app.use(routeNotFound);
+
+// app.use(errorHandler)
+
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
